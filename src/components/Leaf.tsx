4086fe527da35cbf0925f9243687667e0f99ec31
akinipek <aipek@deloitte.nl>
2021-11-07 15:06:25 +0100

    leaf container added

---
+++ src/components/Leaf.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const LeafContainer = styled.div`
+
+`;
+
+const Leaf: React.FC = () => {
+  return (
+    <LeafContainer>
+    
+    </LeafContainer>
+  );
+};
+
+export default Leaf;
